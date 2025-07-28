e3ea8b2c02892e925b8fba00d90dbafe988a1822
andrehebra <52555960+andrehebra@users.noreply.github.com>
2025-07-28 19:21:31 -0400

    set service to chromedriver.exe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-service = Service(executable_path="")
+service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
